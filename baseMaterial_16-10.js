ea900cc94446b56cf09f3b94af6e090331311ba1
eqionux <eqionux@hotmail.com>
2019-10-22 11:11:13 +0800

    Create baseMaterial_16-10.js

---
+++ baseMaterial_16-10.js
@@ -0,0 +1,47 @@
+/*Material Name*/
+var materialName = "TEST MAT 1";
+
+/*Material Code*/
+var materialCode = "TMAT1"
+
+/*Material Image Link*/
+var materialImg = [
+"/files/subscribers/a4ceaf37-e981-4539-8737-dcf01e7d0955/Webfiles/WB/MKE10---White-Foam-Board-10mm-new.jpg",
+"/files/subscribers/a4ceaf37-e981-4539-8737-dcf01e7d0955/Webfiles/WB/MKE10---White-Foam-Board-10mm-new.jpg",
+"/files/subscribers/a4ceaf37-e981-4539-8737-dcf01e7d0955/Webfiles/WB/MKE10---White-Foam-Board-10mm-new.jpg"
+];
+
+/*Material Video Link*/
+var materialVid = [
+"https://upload.botaksign.com/Print%20By%20Phone/TestFiles/mattglossvidresized.mp4"
+];
+
+/*Material General Description*/
+var generalText = [
+	"This is general text line 1",
+	"This is general text line 2",
+	"This is general text line 3"
+];
+
+/*Material Characteristics*/
+var materialText = [
+	"TEST MAT 1 HEADER 1",
+	"TEST MAT 1 HEADER 2",
+	"TEST MAT 1 HEADER 3",
+	"TEST MAT 1 HEADER 4"
+];
+
+/*Material Characteristics Details*/
+var materialTextInfo = [
+	["Material is tear resistant", "Can withstand certain amount of tearing force"],
+	["Material is resistant to UV rays", "Is able to etc"],
+	["Material is smudgeproof", "able to etc"],
+	["Material is weatherproof", "Able to be displayed outdoors for 4 months"]
+]
+
+/*Material Caution Description*/
+var cautionText = [
+	"Please note that there will be a 2 mm tolerance all round for trimming to size",
+	"Print out colour may vary",
+	"Comes with permanent adhesive"
+];
